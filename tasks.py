df448324df277aa1bae7c8177411f181b78b8620
Madhusudhan <madhusudhanrk98@gmail.com>
2021-08-14 19:10:11 +0530

    first commit

---
+++ tasks.py
@@ -0,0 +1,45 @@
+#ODD OR EVEN task1
+#  num = int(input("Enter a number:\n"))
+# if(num%2==0):
+#     print("Number is even")
+# else:
+#     print("Number is odd")
+
+#ODD OR EVEN limit
+
+# limit = int(input("Enter a Limit number:\n"))
+# limit = limit+1
+# for i in range(1,limit):
+#     if(i%2!=0):
+#         print(i)
+
+#STRING REVERSE task2
+
+#Method 1
+# def reverse_string(str):             
+#     strrev = ""
+#     for i in str:
+#         strrev = i + strrev
+#     return strrev
+
+# print(reverse_string(input("Enter any thing:\n")))
+
+#Method 2
+
+# def reverse_string(str): 
+#     strrev = str[::-1]   
+#     return strrev         
+# print(reverse_string(input("Enter any thing:\n")))
+
+#SWAPPING task3
+# a=3
+# b=4
+# t=""
+# print("Before Swap:",a,b)
+# t = a
+# a = b
+# b = t
+# print("After Swap:",a,b)
+
+#Bubble sort
+
