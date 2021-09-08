39149d944d95764de31e0f9a4c7d28990224086a
Madhusudhan <madhusudhanrk98@gmail.com>
2021-09-08 19:39:14 +0530

    finishing with datatypes

--- datatypes.py
+++ datatypes.py
@@ -17,11 +17,12 @@ ALL ARE SEQUENCE DATA TYPES 1 BY 1
 8. Dictionary -> a = {'key':'value'} Map in other languages
 """
 
-# None
+# None***********
+
 a = "" #no value assigned so it is None or null
 print(a)
 
-#Numeric
+#Numeric**********
 a = 5
 print(type(a)) # INT type value 5
 
@@ -32,17 +33,25 @@ a = complex(5,6) # complex(val1,val2*j)
 print(type(a)) # COMPLEX type 
 print(a) # value 5+6j
 
-#BOOL
+#BOOL**********
 
 print(6>7) # o/p False
 
 print(int(True)) # o/p 1 TRUE = 1, False = 0 if type converted to numeric
 
 # SEQUENCE DATA TYPES (ONLY ABOUT RANGE EXCEPT WILL BE DISCUSSED DETAILED)
+
+
+# RANGE********* 
+
 # syntax range(start,stop,steps) and stop-1 because range starts from 0 like arrays
 
 a = list(range(0,10)) # range return range object so converting to list
 print(a) # 0 to 9 because it like array start from 0
 a = list(range(0,10,2)) # skip 2-1=> 1 step 
 print(a)
+
+
+#DICTIONARY detailed explained
+
 
