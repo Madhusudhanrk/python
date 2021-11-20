ffd4d9ae6e9066aa4003fe9112802bc52bab4a40
madhusudhan <madhusudhanrk98@gmail.com>
2021-11-20 19:10:18 +0530

    bitwise and number systems

---
+++ 10_bitwise.py
@@ -0,0 +1,13 @@
+#INTRO TO Bitwise **********************************************************************
+
+
+"""
+Bitwise 6 types
+
+1. & and           => 1001 & 1000 =>1000 like and 2 numbers true
+2. | or            => 1001 & 1000 =>1001 like or  1 numbers true
+3. ^ xor           => 1001 & 1000 =>0001 like xor  1 numbers odd means 1 or 0
+4. ~ compliment    => 1001        =>0110 full reverse
+5. << left Shift   => 10<<2       =>00001010->00101000 remove 2 digits from left and add 2 0's to right
+6. >> Right Shift  => 10>>2       =>00001010->00000010 remove 2 digits from right and add 2 0's to left
+"""
---
+++ 9_number system.py
@@ -0,0 +1,23 @@
+#INTRO TO Number system **********************************************************************
+
+
+#four types 
+"""
+1. Binary Base 2       -> 1 or 0
+2. Decimal Base 10     -> 0 to 9
+3. Octal Base 8        -> 0 to 8
+4. Hexadecimal Base 16 -> 0 to 9 and A-F
+"""
+val = 40
+ans = bin(40)
+print(ans)#o/p->0b101000 ->0b is for binary
+
+ans = oct(40)
+print(ans)#o/p->0o50 ->0o is for octa
+
+ans = hex(40)
+print(ans)#o/p->0x28 ->0x is for Hexa
+
+"""
+conversion example pending
+"""
