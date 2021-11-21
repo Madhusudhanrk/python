f24dde20da435a29715a4d39df62c7c6c2ce485f
madhusudhan <madhusudhanrk98@gmail.com>
2021-11-22 05:27:28 +0530

    special variables

--- special_variable.py
+++ special_variable.py
@@ -2,7 +2,7 @@ print(__name__) #directly this file running means it is __main__
                 # if it is running as a module in another file it is file name 27_special_variable
 
 def greetings():
-    print("hello madhu")
+    print("hello madhu from",__name__)
 
 if __name__ == "__main__":
     greetings()
--- support_special_var.py
+++ support_special_var.py
@@ -1,3 +1,4 @@
 import special_variable
 
 print("we are in support_special_var")
+print(special_variable.greetings())#o/p hello madhu from special_variable because __name__ holds module name 
